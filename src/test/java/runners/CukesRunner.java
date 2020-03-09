97a8a9f2c8de0da047a238cfc9b72049369daa2f
popova.om@gmail.com <1@Np$Pd?o+?+>
2020-03-09 17:43:19 -0500

    jenkins try 3

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        plugin = "html:target/cucumber-reports",
+        plugin = {"html:target/cucumber-reports",
+                "json:target/cukesreport.json"},
         features = "src/test/resources/features",
         glue = "step_definitions",
         dryRun =false,
