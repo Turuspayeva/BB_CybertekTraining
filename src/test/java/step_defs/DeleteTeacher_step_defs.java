1f8357372f50748ca295420138e5f2b8c520721c
Nurgul duishebaeva <duishebaevanuku@gamil.com>
2020-03-12 17:40:02 -0500

    delete teacher 1

--- src/test/java/step_defs/DeleteTeacher_step_defs.java
+++ src/test/java/step_defs/DeleteTeacher_step_defs.java
@@ -3,14 +3,15 @@ package step_defs;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.junit.Test;
-import pages.AddTeacherPage;
-import pages.AllTeacherPage;
-import pages.DeleteTeacherPage;
-import pages.LeftSideMenu;
+import pages.*;
 import utilities.*;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class DeleteTeacher_step_defs {
 
@@ -18,11 +19,13 @@ public class DeleteTeacher_step_defs {
     LeftSideMenu leftSideMenu =new LeftSideMenu();
     DeleteTeacherPage deleteTeacherPage = new DeleteTeacherPage();
     AddTeacherPage addTeacherPage = new AddTeacherPage();
+    EditTeacherPage editTeacherPage= new EditTeacherPage();
+    Random random = new Random();
 
-            @Test
-       @Given("User will go on main page")
+    @Given("User will go on main page")
          public void user_will_go_on_main_page() {
                 Driver.getDriver().get(Config.getProperty("url"));
+
 
             }
                @When("User Should Press on Teachers")
@@ -33,17 +36,30 @@ public class DeleteTeacher_step_defs {
 
     @When("User Clicks on All Teacher")
     public void user_Clicks_on_All_Teacher() {
-   deleteTeacherPage.AllTeacher.click();
+    deleteTeacherPage.AllTeacher.click();
+    deleteTeacherPage.teacherCards.get(random.nextInt(deleteTeacherPage.teacherCards.size())).click();
+    TempStorage.addDataToTempStorage("teacher_id",deleteTeacherPage.teacherIdInDetailPage.getText());
+        System.out.println(TempStorage.getDataFromTempStorage("teacher_id"));
+        Driver.getDriver().navigate().back();
+        SeleniumUtils.waitForVisibility(deleteTeacherPage.teacherInputId, 10);
+        deleteTeacherPage.teacherInputId.clear();
+       deleteTeacherPage.teacherInputId.sendKeys(TempStorage.getDataFromTempStorage("teacher_id"));
+       deleteTeacherPage.searchButton.click();
+
+        SeleniumUtils.pause(3);
+
 }
 
    @When("Clicks On The Three dots at the Right")
        public void clicks_On_The_Three_dots_at_the_Right() {
-                deleteTeacherPage.threeDots.click();
+        editTeacherPage.threePoints.click();
+        SeleniumUtils.pause(3);
 
     }
 
     @When("User Should Click on Delete Button")
     public void user_Should_Click_on_Delete_Button() {
+        SeleniumUtils.pause(2);
         deleteTeacherPage.delete.click();
 
     }
@@ -56,14 +72,18 @@ public class DeleteTeacher_step_defs {
    @Then("User will check table in DataBAse")
     public void user_will_check_table_in_DataBAse() {
 
-
        try {
            DBUtility.createConnection();
-       } catch (
-               SQLException e) {
+       } catch (SQLException e) {
            e.printStackTrace();
        }
-       String query = "select * from teacher  where  =";
+  String query = "select * from teacher where teacher_id = " + TempStorage.getDataFromTempStorage("teacher_id");
+       try {
+           List<Map<Object, Object>> result =  DBUtility.executeQuery(query);
+           Assert.assertTrue("Teacher  id still exist in DB", result.isEmpty());
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
 
 
    }
